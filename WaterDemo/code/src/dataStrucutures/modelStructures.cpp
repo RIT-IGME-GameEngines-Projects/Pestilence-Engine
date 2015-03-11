5b2935ebd8c02ef4d079acc222b0ddc56b5355a4
Jake Higgins <jth7036@rit.edu>
2015-03-11 15:58:23 -0400

    Updated model data structures

--- WaterDemo/code/src/dataStrucutures/modelStructures.cpp
+++ WaterDemo/code/src/dataStrucutures/modelStructures.cpp
@@ -1,22 +1,23 @@
 struct Point {
-	float* x;
-	float* y;
-	float* z;
+	float x;
+	float y;
+	float z;
+	float w;
 };
 
 struct UV {
-	float* u;
-	float* v;
+	float u;
+	float v;
 };
 
 struct Normal {
-	float* x;
-	float* y;
-	float* z;
+	float x;
+	float y;
+	float z;
 };
 
 struct Face {
 	Point* points;
 	UV* uvs;
-	Normal normal;
+	Normal* normals;
 };

fb20f52664ac49456df9483cca8fe6f3a4dac3e9
unknown <gmb9280@GDD-10.main.ad.rit.edu>
2015-03-31 10:45:36 -0400

    Added some helper methods to Logger.h

--- PestilenceEngine/code/src/logger.h
+++ PestilenceEngine/code/src/logger.h
@@ -1,16 +1,67 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include "MyVector.h"
+
 // Enum to set flag for which 
-enum LOG_FLAG = { ALL, RELEASE, GRAPHICS, MEMORY, SCHEDULING, AUDIO, AI };
+enum LOG_FLAG { ALL, RELEASE, GRAPHICS, MEMORY, SCHEDULING, AUDIO, AI };
 
 class Logger
 {
 public: 
 	// Default constructor
-	Logger(); 
+	Logger()
+	{
+		flags = new MyVector<LOG_FLAG>();
+
+	};
 private: 
-	MyVector<LOG_FLAG> flags; 
+	// Pointer to a MyVector - stored on heap
+	MyVector<LOG_FLAG>* flags; 
+
+	// Add a flag to the list of flags to log. 
+	void addFlag(LOG_FLAG lf)
+	{
+		// If the flag is ALL, remove everything in vector and replace with ALL
+		if (lf == ALL)
+		{
+			flags->Release();
+			flags->Push(lf);
+		}
+		else
+		{
+			flags->Push(lf);
+		}
+	};
+
+	// Remove a flag from the list
+	void removeFlag(LOG_FLAG lf)
+	{
+		for (int i = 0; i < flags->GetSize(); i++)
+		{
+			if (flags->GetCopyAt(i) == lf)
+			{
+				// Remove it
+				flags->RetrieveByIndex(i);
+				return; // No more!
+			}
+		}
+	};
+
+	void clearFlags()
+	{
+		flags->Release();
+	};
+
+	// Check to see if the Logger has a specific flag
+	bool hasFlag(LOG_FLAG lf)
+	{
+		for (int i = 0; i < flags->GetSize(); i++)
+		{
+			if (flags->GetCopyAt(i) == lf) return true;
+		}
+		return false;
+	}
 };
 
 

6096493c0e8a22a4e3dcecdf8cb84fcecb27aed4
unknown <gmb9280@GDD-1.main.ad.rit.edu>
2015-04-03 15:28:00 -0400

    -Changed to struct and enum system for Logger.

--- PestilenceEngine/code/src/logger.h
+++ PestilenceEngine/code/src/logger.h
@@ -4,7 +4,7 @@
 #include "MyVector.h"
 
 // Enum to set flag for which 
-enum LOG_FLAG { ALL, RELEASE, GRAPHICS, MEMORY, SCHEDULING, AUDIO, AI };
+enum LOG_FLAG { _ALL, _RELEASE, _GRAPHICS, _MEMORY, _SCHEDULING, _AUDIO, _AI };
 
 class Logger
 {
@@ -12,55 +12,52 @@ public:
 	// Default constructor
 	Logger()
 	{
-		flags = new MyVector<LOG_FLAG>();
 
 	};
 private: 
-	// Pointer to a MyVector - stored on heap
-	MyVector<LOG_FLAG>* flags; 
+	// Keep track of which LOG_FLAGs are flagged. 
+	struct FLAG_LIST
+	{
+		bool ALL = false; 
+		bool RELEASE = false; 
+		bool GRAPHICS = false; 
+		bool MEMORY = false; 
+		bool SCHEDULING = false; 
+		bool AUDIO = false; 
+		bool AI = false; 
+	};
+	FLAG_LIST flags; 
 
 	// Add a flag to the list of flags to log. 
 	void addFlag(LOG_FLAG lf)
 	{
-		// If the flag is ALL, remove everything in vector and replace with ALL
-		if (lf == ALL)
+		switch (lf)
 		{
-			flags->Release();
-			flags->Push(lf);
-		}
-		else
-		{
-			flags->Push(lf);
+			case (_ALL) : clearFlags();  flags.ALL = true; break;
+			case (_RELEASE) : clearFlags();  flags.RELEASE = true; break;
+			case (_GRAPHICS) : flags.GRAPHICS = true; break; 
+			case (_MEMORY) : flags.MEMORY = true;  break;
+			case (_SCHEDULING) : flags.SCHEDULING = true; break;
+			case (_AUDIO) : flags.AUDIO = true;
+			case(_AI) : flags.AI = true;  break;
 		}
 	};
 
 	// Remove a flag from the list
 	void removeFlag(LOG_FLAG lf)
 	{
-		for (int i = 0; i < flags->GetSize(); i++)
-		{
-			if (flags->GetCopyAt(i) == lf)
-			{
-				// Remove it
-				flags->RetrieveByIndex(i);
-				return; // No more!
-			}
-		}
+		
 	};
 
 	void clearFlags()
 	{
-		flags->Release();
+		
 	};
 
 	// Check to see if the Logger has a specific flag
 	bool hasFlag(LOG_FLAG lf)
 	{
-		for (int i = 0; i < flags->GetSize(); i++)
-		{
-			if (flags->GetCopyAt(i) == lf) return true;
-		}
-		return false;
+		
 	}
 };
 

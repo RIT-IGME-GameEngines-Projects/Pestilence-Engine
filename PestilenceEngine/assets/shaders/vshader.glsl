e605c0a894a890bdfdeb06d6754b948ab2971d8f
Jake Higgins <jth7036@rit.edu>
2015-05-04 13:54:45 -0400

    Added shader framework for lighting

--- PestilenceEngine/assets/shaders/vshader.glsl
+++ PestilenceEngine/assets/shaders/vshader.glsl
@@ -1,15 +1,45 @@
 #version 330 core
 
-layout(location = 0) in vec3 vertexPosition_modelspace;
-layout(location = 1) in vec2 vertexUV;
+// m_ = modelspace
+// w_ = worlspace
+// c_ = cameraspace
+
+layout(location = 0) in vec3 m_position;
+layout(location = 1) in vec2 m_uv;
+layout(location = 2) in vec3 m_normal;
 
 out vec2 UV;
+out vec3 w_position;
+out vec3 c_normal;
+out vec3 c_eyeDir;
+out vec3 c_lightDir;
 
 uniform mat4 MVP;
+uniform mat4 V;
+uniform mat4 M;
+uniform vec3 w_lightPos;
 
 void main()
 {   
-	gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
+	// Output position
+	gl_Position = MVP * vec4(m_position, 1);
 
-	UV = vertexUV;
+	// Position of vertex in worldspace
+	w_position = (M * vec4(m_position,1)).xyz;
+
+	// Vector from vertex to camera
+	vec3 c_position = (V * M * vec4(m_position, 1)).xyz;
+	c_eyeDir = vec3(0,0,0) - c_position;
+
+	// Vector from vertex to light
+	vec3 c_lightPos = (V * vec4(w_lightPos, 1)).xyz;
+	c_lightDir = c_lightPos + c_eyeDir;
+
+	// Normal of the vertex
+	c_normal = (V * M * vec4(m_normal,0)).xyz;
+
+	// UV of vertex
+	UV = vUV;
 }
+
+// REFERENCE: https://code.google.com/p/opengl-tutorial-org/source/browse/tutorial08_basic_shading/StandardShading.vertexshader
